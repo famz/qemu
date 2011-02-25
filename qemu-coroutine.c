e215321e5832eea50ac7ac1528e02eb50146292d
Kevin Wolf <kwolf@redhat.com>
2011-02-25 16:50:12 +0100

    coroutine: Fix mismerge
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- qemu-coroutine.c
+++ qemu-coroutine.c
@@ -113,8 +113,8 @@ bool qemu_co_queue_next(CoQueue *queue)
     next = QTAILQ_FIRST(&queue->entries);
     if (next) {
         QTAILQ_REMOVE(&queue->entries, next, co_queue_next);
-        qemu_coroutine_enter(next, NULL);
-        assert(qemu_in_coroutine());
+        QTAILQ_INSERT_TAIL(&unlock_bh_queue, next, co_queue_next);
+        qemu_bh_schedule(unlock_bh);
     }
 
     return (next != NULL);
