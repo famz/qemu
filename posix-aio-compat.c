9cadbb123a04741933c4c12a20b14db2349b5a95
Kevin Wolf <kwolf@redhat.com>
2011-05-02 17:32:12 +0200

    tmp

--- posix-aio-compat.c
+++ posix-aio-compat.c
@@ -390,7 +390,7 @@ static void qemu_paio_submit(struct qemu_paiocb *aiocb)
     aiocb->ret = -EINPROGRESS;
     aiocb->active = 0;
     mutex_lock(&lock);
-    if (/*idle_threads == 0 &&*/ cur_threads < max_threads)
+    if (idle_threads == 0 && cur_threads < max_threads)
         spawn_thread();
     QTAILQ_INSERT_TAIL(&request_list, aiocb, node);
     mutex_unlock(&lock);
