f602035a881364c12b50680fc91a286eb60ce0f4
Fam Zheng <famz@redhat.com>
2014-01-31 15:49:10 +0100

    module: implement module loading
    This patch adds loading, stamp checking and initialization of modules.
    
    The init function of dynamic module is no longer directly called as
    __attribute__((constructor)) in static linked version, it is called
    only after passed the checking of presense of stamp symbol:
    
        qemu_stamp_$RELEASEHASH
    
    where $RELEASEHASH is generated by hashing version strings and content
    of configure script.
    
    With this, modules built from a different tree/version/configure will
    not be loaded.
    
    The module loading code requires gmodule-2.0.
    
    Modules are searched under
     - CONFIG_MODDIR
     - executable folder (to allow running qemu-{img,io} in the build
       directory)
     - ../ of executable folder (to allow running system emulator in the
       build directory)
    
    Modules are linked under their subdir respectively, then copied to top
    level of build directory for above convinience, e.g.:
        $(BUILD_DIR)/block/curl.so -> $(BUILD_DIR)/block-curl.so
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/qemu/module.h
+++ include/qemu/module.h
@@ -14,11 +14,42 @@
 #ifndef QEMU_MODULE_H
 #define QEMU_MODULE_H
 
+/* Using glue() causes the preprocessor to choke on values of CONFIG_STAMP
+ * that start with a digit, because they are split at the first
+ * letter.  For example:
+ *
+ *      config-host.h:33:22: error: invalid suffix "c2a05f88b8817d3a0a961f0d8296751d21e8774" on integer constant
+ *      #define CONFIG_STAMP 1c2a05f88b8817d3a0a961f0d8296751d21e8774
+ *                           ^
+ *      include/qemu/module.h:20:48: note: in expansion of macro 'CONFIG_STAMP'
+ *      #define DSO_STAMP_FUN         glue(qemu_stamp_,CONFIG_STAMP)
+ *                                                     ^
+ */
+#define DSO_STAMP_FUN2(x)     qemu_stamp_##x
+#define DSO_STAMP_FUN1(x)     DSO_STAMP_FUN2(x)
+#define DSO_STAMP_FUN         DSO_STAMP_FUN1(CONFIG_STAMP)
+#define DSO_STAMP_FUN_STR     stringify(DSO_STAMP_FUN)
+
+#ifdef BUILD_DSO
+void DSO_STAMP_FUN(void);
+/* This is a dummy symbol to identify a loaded DSO as a QEMU module, so we can
+ * distinguish "version mismatch" from "not a QEMU module", when the stamp
+ * check fails during module loading */
+void qemu_module_dummy(void);
+
+#define module_init(function, type)                                         \
+static void __attribute__((constructor)) do_qemu_init_ ## function(void)    \
+{                                                                           \
+    register_dso_module_init(function, type);                               \
+}
+#else
 /* This should not be used directly.  Use block_init etc. instead.  */
 #define module_init(function, type)                                         \
-static void __attribute__((constructor)) do_qemu_init_ ## function(void) {  \
+static void __attribute__((constructor)) do_qemu_init_ ## function(void)    \
+{                                                                           \
     register_module_init(function, type);                                   \
 }
+#endif
 
 typedef enum {
     MODULE_INIT_BLOCK,
@@ -34,6 +65,7 @@ typedef enum {
 #define type_init(function) module_init(function, MODULE_INIT_QOM)
 
 void register_module_init(void (*fn)(void), module_init_type type);
+void register_dso_module_init(void (*fn)(void), module_init_type type);
 
 void module_call_init(module_init_type type);
 
