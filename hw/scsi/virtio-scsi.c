f60b3c41d5439dbe8961b7e1576774e1824b607d
Fam Zheng <zhengfeiran@bytedance.com>
2019-10-22 16:16:48 +0800

    virtio-scsi

--- hw/scsi/virtio-scsi.c
+++ hw/scsi/virtio-scsi.c
@@ -852,6 +852,7 @@ static void virtio_scsi_hotunplug(HotplugHandler *hotplug_dev, DeviceState *dev,
         virtio_scsi_acquire(s);
         /* If other users keep the BlockBackend in the iothread, that's ok */
         blk_set_aio_context(sd->conf.blk, qemu_get_aio_context(), NULL);
+        object_unparent(OBJECT(dev));
         virtio_scsi_release(s);
     }
 }
